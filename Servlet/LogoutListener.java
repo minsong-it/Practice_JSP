927fd7834ceca016f9a129c8596f1d95d51fca67
Minsong <113004260+minsong-it@users.noreply.github.com>
2022-10-12 09:17:17 +0900

    Listener API
    listener api를 활용해 웹 페이지의 접속 아이디와 접속 상태를 출력하고 활용할 수 있음

---
+++ Servlet/LogoutListener.java
@@ -0,0 +1,55 @@
+package sec04.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutTest
+ */
+@WebServlet("/LogoutListener")
+public class LogoutListener extends HttpServlet {
+	
+	ServletContext context;
+	
+    public LogoutListener() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doHandle(request, response);
+	}
+
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		context = getServletContext();
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		String user_id = request.getParameter("user_id");
+		session.invalidate();
+		List user_list = (ArrayList) context.getAttribute("user_list");
+		user_list.remove(user_id);
+		context.removeAttribute("user_list");
+		context.setAttribute("user_list", user_list);
+		out.println("<br> 로그아웃했습니다. <br>");
+		out.println("<a href='login.html'>돌아가기</a>");
+		
+	}
+}
